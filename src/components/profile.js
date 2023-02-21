1093dd5b1877886eefc3be4079332ded162b7537
feindreh <119032218+feindreh@users.noreply.github.com>
2023-02-21 12:32:45 +0100

    init

---
+++ src/components/profile.js
@@ -0,0 +1,17 @@
+import React,{ Component } from "react";
+
+class Profile extends Component{
+  constructor(){
+    super();
+  }
+
+
+  render(){
+    return(
+      <>Profile</>
+    )
+  }
+  
+}
+
+export default Profile;
